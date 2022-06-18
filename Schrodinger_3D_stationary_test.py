b9bfe25c2bf01bf443800a477887de4dc867ee7d
Natalia <nkram@wp.pl>
2022-06-18 21:10:50 +0200

    Corrected 3Dtest

--- Schrodinger_3D_stationary_test.py
+++ Schrodinger_3D_stationary_test.py
@@ -21,10 +21,10 @@ pi = np.pi
 time  = 0
 nx = 3
 ny = 3
-L = 1**-10
+L = 1
 x = np.linspace(0, L, 100)
 y = np.linspace(0, L, 100)
-X, Y = np.meshgrid(x, y)
+X,Y= np.meshgrid(x, y)
 
 Ex = nx**2*hbc**2*np.pi**2/(2*m*L**2)
 Ey = ny**2*hbc**2*np.pi**2/(2*m*L**2)
@@ -36,32 +36,26 @@ def Psi_3d(a, b, t):
     psi_3d = np.real((2/np.sqrt(L*L))*np.sin(nx*pi*a/L)*np.sin(ny*pi*b/L)*np.exp(-1j*E*t/(hbc**2)))  
     return psi_3d	
 	
-psi = np.array([Psi_3d(x,y,time) for x,y in zip(np.ravel(X), np.ravel(Y))])
-Z = Psi_3d(X,Y,time)
-PSI = psi.reshape(X.shape)
-#plot = none
 def animate(i, Z, line):
     global time
-    #psi = np.array([Psi_3d(x,y,time) for x,y in zip(np.ravel(X), np.ravel(Y))])
-    #PSI = psi.reshape(X.shape)
+    Z = Psi_3d(X, Y, time)
     ax.clear();
-    Z = Psi_3d(X,Y,time)
-    line = ax.plot_surface(X,Y,Z, cmap = cm.twilight, linewidth =0.5)
+    ax.set_zlim(-2, 2)
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(r'$y$')
+    line = ax.plot_surface(X, Y, Z, cmap = cm.twilight, linewidth =0.5)
     time = time+300
-    print("Tu")
+    #print("tu")
     return line,
+	
 if __name__ == "__main__":
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    line = ax.plot_surface(X,Y,Z,cmap = cm.twilight, linewidth = 0.5)
-    plt.xlabel('X')
-    plt.ylabel('Y')
     ax.set_zlim(-2, 2)
-    ax.set_zlabel('Z')
-    Z = Psi_3d(X,Y,time)
-    plt.title('Probability density n=1', color='magenta')
-    
-    ani = animation.FuncAnimation(fig, animate, fargs = (Z, line),  interval=100, blit = FALSE)
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(r'$y$')
+    Z = Psi_3d(X, Y, time)
+    line = ax.plot_surface(X, Y, Z, cmap = cm.twilight, linewidth = 0.5)
+    plt.title('2D infinite sqare well stationary states', color='black')
+    ani = animation.FuncAnimation(fig, animate, fargs = (Z, line),  interval=50)
     plt.show()
-	
-	
